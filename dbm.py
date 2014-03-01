7b81f77fb3971a153156488ec7ad735258d9b956
Chris Bonnell <christopher.p.bonnell@gmail.com>
2014-02-28 18:49:23 -0600

    Better. Enforced greedy, layerwise CD training, implemented a deterministic scorer, and changed the test case to something not XOR (oops, forgot that one was too hard)

--- dbm.py
+++ dbm.py
@@ -6,7 +6,7 @@ class DBM(object):
     # fantasy_count: The number of markov chains to run in the background
     # learning_rate: starting learning rate. Will be continued harmonically from the starting value.
     def __init__(self,dataset,labels=numpy.array([]),
-                batch_size = 500,
+                batch_size = 50,
                 layers=[10,2],
                 fantasy_count = 10,
                 learning_rate = .0001, ):
@@ -24,7 +24,8 @@ class DBM(object):
                             'size':self.features,
                             'fantasy': numpy.random.randint(0,2,(fantasy_count,self.features)).astype(float),
                             'mu':0,
-                            'bias': numpy.zeros((1,self.features))})
+                            'bias':self.sigma_inverse(numpy.mean(dataset, axis=0)).reshape(1,self.features),
+                            })
         for layer in range(len(layers)):
             size = layers[layer]
             hidden = {'layer':'hidden '+str(layer), 'size':size, 'mu':0}
@@ -34,7 +35,7 @@ class DBM(object):
             hidden['momentum'] = numpy.zeros((above,size))
             hidden['fantasy'] = numpy.random.randint(0,2,(fantasy_count,size)).astype(float)
             self.layers.append(hidden)
-        
+         
     
     #Stochastic annealing scheduler. This one assures that, regardless of
     #  starting value the sequence is in l^2-l^1
@@ -44,15 +45,18 @@ class DBM(object):
     
     def l2_pressure(self,weights):
         norms = numpy.sqrt(numpy.sum(weights*weights, axis=0))
-        norms = norms.reshape(norms.shape[0],1)
+        norms = numpy.floor(1/norms.reshape(norms.shape[0],1))
         norms = norms.T.repeat(weights.shape[0], axis=0)
-        return -.01*norms*weights
+        out = norms*weights
+        return -.01*out
 
     #some sigmoid function, this one is fine.
     def sigma(self, x):
         x = numpy.clip(x,-100,100)
         return 1/(1+numpy.exp(-x))
-
+    def sigma_inverse(self,x):
+        x = numpy.clip(x,.00001,.99999)
+        return numpy.log(x/(1-x))
     
     #Quick and dirty bootstrapper to manage samples per epoch
     def data_sample(self, num):
@@ -95,7 +99,7 @@ class DBM(object):
     
     #The energy of the whole DBM with given inputs and hidden activations
     def internal_energy(self, v, hs):
-        temp=self._energy(v, self.layers[1]['W'], hs[0],numpy.zeros((1,v.shape[1])),self.layers[1]['bias'])
+        temp=self._energy(v, self.layers[1]['W'], hs[0],self.layers[0]['bias'],self.layers[1]['bias'])
         for i in range(1, len(self.layers)-1):
             temp += self._energy(hs[i-1], self.layers[i+1]['W'], hs[i], self.layers[i]['bias'], self.layers[i+1]['bias'])
         return temp
@@ -123,61 +127,40 @@ class DBM(object):
     
     # prob_given_vis gives a vector of length j with the corresponding probs
     # subset to theappropriate entry to get hj1==1
-    def prob_given_vis(self, W, vs,bias):
-        return self.sigma(bias + numpy.dot(vs, W))
+    def prob_given_vis(self, W, vs,bias, double=False):
+        if double:
+            return self.sigma(2*(bias + numpy.dot(vs, W)))
+        else:
+            return self.sigma(bias + numpy.dot(vs, W))
 
 
     #prob_given_out is the same as above, but with the opposite value  and convention.
-    def prob_given_out(self, W, hs,bias):
-        return self.sigma(bias + numpy.dot( hs, W.T))
+    def prob_given_out(self, W, hs,bias,double=False):
+        if double:
+            return self.sigma(2*(bias + numpy.dot(hs, W.T)))
+        else:
+            return self.sigma(bias + numpy.dot( hs, W.T))
 
 
     #Tiny gibbs sampler for the fantasy particle updates. The numer of iterations could be controlled, but needn't be
-    def gibbs_update(self, gibbs_iterations=100):
-        layers = len(self.layers)
+    def gibbs_update(self, gibbs_iterations=10, layers = None):
+        if layers == None:
+            layers = len(self.layers)
         for j in range(gibbs_iterations):
             for i in range(1,layers):
+                double = bool(i%2) 
                 active = self.layers[i-1]['fantasy']
                 bias = self.layers[i]['bias']
                 W = self.layers[i]['W']
-                self.layers[i]['fantasy'] = self.sample(self.prob_given_vis, (W,active,bias))
+                self.layers[i]['fantasy'] = self.sample(self.prob_given_vis, (W,active,bias,double))
             for i in range(layers-1,1,-1):
+                double = not bool(i%2) 
                 active = self.layers[i]['fantasy']
                 bias = self.layers[i-1]['bias']
                 W = self.layers[i]['W']
-                self.layers[i-1]['fantasy'] = self.sample(self.prob_given_out,(W,active,bias))
+                self.layers[i-1]['fantasy'] = self.sample(self.prob_given_out,(W,active,bias, double))
 
             
-
-
-    #This step does the boltzmann part.
-    def unsupervised_step(self, data, labels,rate):
-        layers=len(self.layers)
-        # You could train the last layer too, 
-        # but as the last layer communicates the
-        # supervised results, this might not help you 
-        # so much as just ruin all predictions.
-        for i in range(1,layers-1):
-            if i==1:
-                previous = data
-            else:
-                previous = self.layers[i-1]['mu']
-            bias = self.layers[i]['bias']
-            mu = bias+numpy.dot(previous,self.layers[i]['W'])
-            #I came up with this bias update scheme. It's not actually
-            #in the papers, but it seems reasonable.
-            bias_part = mu.mean(axis=0).reshape(*bias.shape)
-            self.layers[i]['bias'] = bias + rate*(bias_part-bias)
-            mu = self.sigma(mu)
-            self.layers[i]['mu'] = mu 
-            gradient_part = - 1.0/(self.datapts*self.batch_size) * numpy.dot(previous.T, mu)
-            approx_part =- 1.0/self.fantasy_count * numpy.dot(self.layers[i-1]['fantasy'].T,
-                                                              self.layers[i]['fantasy'])
-            self.layers[i]['W'] =( self.layers[i]['W'] 
-                                  + rate *gradient_part 
-                                  + rate *approx_part)
-
-
     #This is stochastic gradient descent version of a dropout back-propagator.
     def dropout_step(self,data,labels,rate, 
                      dropout_fraction = 0.5, momentum_decay = 0):
@@ -221,15 +204,36 @@ class DBM(object):
 
 
     #Train, or continue training the model according to the training schedule for another train_iterations iterations
-    def train_unsupervised(self, train_iterations=10000, gibbs_iterations=10):
+    def train_unsupervised(self, layer, train_iterations=10000, gibbs_iterations=10):
+        layers=len(self.layers)
         for iter in range(train_iterations):
-            self.gibbs_update(gibbs_iterations)
+            self.gibbs_update(gibbs_iterations,layer)
             data, labels = self.data_sample(self.batch_size)
             rate = self.learning_rate
-            self.unsupervised_step(data,labels,rate)
             self.learning_rate=self.next_learning_rate(self.learning_rate)
+            
+            previous = numpy.round(self.predict_probs(data, omit_layers=layers-layer))
+            bias = self.layers[layer]['bias']
+            mu = bias+numpy.dot(previous,self.layers[layer]['W'])
+            #I came up with this bias update scheme. It's not actually
+            #in the papers, but it seems reasonable.
+            bias_part = mu.mean(axis=0).reshape(*bias.shape)
+            self.layers[layer]['bias'] = bias + rate*(bias_part-bias)
+            if layer%2==0:
+                mu = self.sigma(2*mu)
+            else:
+                mu = self.sigma(mu)
+            self.layers[layer]['mu'] = mu 
+            gradient_part = - 1.0/(self.datapts*self.batch_size) * numpy.dot(previous.T, mu)
+            approx_part =- 1.0/self.fantasy_count * numpy.dot(self.layers[layer-1]['fantasy'].T,
+                                                              self.layers[layer]['fantasy'])
+            W =( self.layers[layer]['W'] 
+                + rate *gradient_part 
+                + rate *approx_part)
+            self.layers[layer]['W'] = W + self.l2_pressure(W)
 
     
+
     #Assuming the data came in with labels, which were disregarded during the unsupervised training.
     def train_dropout(self, train_iterations=10000, weight=1):
         layers=len(self.layers)
@@ -247,19 +251,21 @@ class DBM(object):
     #to be set by the mask(==1) and the unknowns will be in flux.
     #the averages of the output values should tell you something.
     #If the mask is none, it will just make up data given your inputs.
-    def gibbs_predict(self, input, mask=None,samples = 100,  gibbs_iterations=100):
-        input_state = {0:input}
+    def gibbs_predict(self, input, mask=None,samples = 100,  gibbs_iterations=100, stop_layer = None):
         layers = len(self.layers)
-        for i in range(1,layers):
+        input_state = {0:input}
+        if stop_layer is None:
+            stop_layer = layers-3
+        for i in range(1,stop_layer+2):
             input_state[i] = numpy.zeros((input.shape[0],self.layers[i]['W'].shape[1]))
         out = []
         for j in range(gibbs_iterations*samples):
-            for i in range(1,layers-1):
+            for i in range(1,stop_layer-1):
                 active = input_state[i-1]
                 bias = self.layers[i]['bias']
                 W = self.layers[i]['W']
                 input_state[i] = self.sample(self.prob_given_vis, (W,active,bias))
-            for i in range(layers-2,0,-1):
+            for i in range(stop_layer-2,0,-1):
                 active = input_state[i+1]
                 bias = self.layers[i]['bias']
                 W = self.layers[i+1]['W']
@@ -274,4 +280,29 @@ class DBM(object):
                 out.append(input_state[0])
         return out
 
-     
+    def deterministic_predict(self, input, mask=None,  stop_layer = None):
+        layers = len(self.layers)
+        input_state = {0:input}
+        if stop_layer is None:
+            stop_layer = layers-3
+        for i in range(1,stop_layer+2):
+            input_state[i] = numpy.zeros((input.shape[0],self.layers[i]['W'].shape[1]))
+        for i in range(1,stop_layer-1):
+            active = input_state[i-1]
+            bias = self.layers[i]['bias']
+            W = self.layers[i]['W']
+            input_state[i] = numpy.round(self.prob_given_vis(W,active,bias))
+        for i in range(stop_layer-2,0,-1):
+            active = input_state[i+1]
+            bias = self.layers[i]['bias']
+            W = self.layers[i+1]['W']
+            input_state[i] = numpy.round(self.prob_given_out(W,active,bias))
+
+        candidate = self.prob_given_out(self.layers[1]['W'], input_state[1],self.layers[0]['bias'])
+        if mask is not None:
+            input_state[0] = candidate*(1-mask) + input_state[0]*mask 
+        else:
+            input_state[0]=candidate
+        return input_state[0]
+
+    
--- run_me.py
+++ run_me.py
@@ -3,19 +3,22 @@ import numpy as np
 import joblib
 
 
-def render_output(i):
+def render_output(i,k):
     energy.append(dbm_test.total_energy())
-    print 'post-training cycle energy: ', energy[-1]
-    joblib.dump(dbm_test, 'dbm_test')
-    joblib.dump(energy, 'dbm_energy')
+    print 'training layer ',k, ' cycle energy: ', energy[-1]
+    joblib.dump(dbm_test, 'output/dbm_test')
+    joblib.dump(energy, 'output/dbm_energy')
+    W = dbm_test.layers[k]['W']
+    print '\t',W.max(), W.min(), W.mean(), W.std()
+    bias = dbm_test.layers[k]['bias']
+    print '\t', bias.max(), bias.min(), bias.mean(), bias.std()
     if i%10==0:
         data = np.array([[1,1,1]])
         mask = np.array([[1,0,0]])
-        print 'target=[1,0,1] (mask [1,0,0]), prediction = ',np.mean(dbm_test.gibbs_predict(data,mask, samples = 100, gibbs_iterations= 10),axis=0)
-
+        print 'target=[1,1,1] (mask [1,0,0]), prediction = ',np.mean(dbm_test.gibbs_predict(data,mask, samples = 100, gibbs_iterations= 10, stop_layer=k),axis=0), 'predict_2', dbm_test.deterministic_predict(data,mask,stop_layer=k)
 
 dataset = np.round(np.random.rand(1000000, 1))
-dataset = np.append(dataset,1-dataset,axis=1)
+dataset = np.append(dataset,dataset,axis=1)
 dataset = np.append(dataset,np.ones((dataset.shape[0],1)),axis=1)
 print 'dataset shape: ', dataset.shape
 
@@ -24,10 +27,11 @@ energy = []
 entropy = []
 accuracy = []
 print 'initializing model'
-dbm_test=DBM(dataset,layers=[20,10])
-render_output(1)
+dbm_test=DBM(dataset,layers=[30, 20,10])
+render_output(1,1)
 
-for i in range(100000):
-    print 'beginning boltzmann training of model'
-    dbm_test.train_unsupervised()
-    render_output(i)
+for k in range(1,3):
+    for i in range(51):
+        print 'beginning boltzmann training of model'
+        dbm_test.train_unsupervised(k)
+        render_output(i,k)
